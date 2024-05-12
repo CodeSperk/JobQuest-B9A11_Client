8d8b4abd30b6a44f388676fdba72f86079e7cdb5
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-05-13 03:33:36 +0600

    added error page

--- src/components/Pages/Error/ErrorPage.jsx
+++ src/components/Pages/Error/ErrorPage.jsx
@@ -4,10 +4,12 @@ import Button1 from "../../Shared/Buttons/Button1";
 const ErrorPage = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      
-      <Link to="/">
+      <div className="relative text-center space-y-6 md:w-1/2">
+      <img src="https://i.ibb.co/Jyfbwrm/Screenshot-5.png" alt="" className="rounded-xl"/>
+      <Link to="/" className="absolute bottom-4 left-1/2">
       <Button1 name="Back To Home"></Button1>
       </Link>
+      </div>
     </div>
   );
 };
